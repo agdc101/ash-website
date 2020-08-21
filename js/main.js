19209ad9fd42a000ee4f9fdee6e9e3155274f19d
Ashley Campbell <ashleycampbell@MacBook-Pro.lan>
2020-08-21 21:44:53 +0100

    portfolio section

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+const sassDiv = document.querySelector('.sass-project');
+const sassTextOverlay = document.querySelector('.sass-overlay-text');
+const portfolioSection= document.querySelector('.port-sect');
+
+// const showOverlay = (element1, element2) => {
+//     element1.classList.remove('hide');
+//     element1.classList.add('show');
+//     element2.classList.remove('hide');
+//     element2.classList.add('show');
+// }
+// const hideOverlay = (element1, element2) => {
+//     element1.classList.add('hide');
+//     element1.classList.remove('show');
+//     element2.classList.add('hide');
+//     element2.classList.remove('show');
+// }
+
+// portfolioSection.addEventListener('mouseover', (e) => {
+//     if (e.target === sassDiv || e.target === sassTextOverlay) {
+//         showOverlay(sassDiv, sassTextOverlay);
+//         return;
+//     }
+//     else {
+//         hideOverlay(sassDiv, sassTextOverlay);
+//         return;
+//     }
+// })
