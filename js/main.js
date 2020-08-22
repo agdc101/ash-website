4e0929b93f5ccd819d8792891c80101b07b9a551
Ashley Campbell <ashleycampbell@macbook-pro.home>
2020-08-22 17:12:35 +0100

    portfolio modal

--- js/main.js
+++ js/main.js
@@ -1,28 +1,43 @@
 
-const sassDiv = document.querySelector('.sass-project');
-const sassTextOverlay = document.querySelector('.sass-overlay-text');
-const portfolioSection= document.querySelector('.port-sect');
+const modalHeader = document.querySelector('.modal-header');
+const modalBody = document.querySelector('.modal-body');
+const dashboardDiv = document.querySelector('.dashboard-portfolio');
+let modalHTML = '';
+let modalHeaderHTML = ``;
 
-// const showOverlay = (element1, element2) => {
-//     element1.classList.remove('hide');
-//     element1.classList.add('show');
-//     element2.classList.remove('hide');
-//     element2.classList.add('show');
-// }
-// const hideOverlay = (element1, element2) => {
-//     element1.classList.add('hide');
-//     element1.classList.remove('show');
-//     element2.classList.add('hide');
-//     element2.classList.remove('show');
-// }
+const projects = [
+    {
+        title: 'SASS Web Layout',
+        imageLink: 'img/screenshots/dashboard.png',
+        description: 'A web app dashboard project built as part of my techdegree with Treehouse,This project is mobile responsive and is layed out using CSS grid methods It also features interactive Javasript graphs local storage and other fun interactions',
+        technologies: 'HTML, CSS.'                   
+    },
+    {
+        title: 'Guess The Movie Title Game',
+        imageLink: 'img/screenshots/game.png',
+        description: 'A javascript driven movie guessing game! The game allows the user 5 lives to guess the correct title. Give it a try!',
+        technologies: 'HTML, CSS, Javascript.'
+    },
+    {
+        title: 'Interactive Photo Gallery',
+        imageLink: 'img/screenshots/gallery.png',
+        description: 'An interactive photo gallery driven using the Jquery framework. Photos can be filtered and searched using a search-bar and a Jquery plug-in is used for the carousel modal',
+        technologies: 'HTML, CSS, Jquery.'
 
-// portfolioSection.addEventListener('mouseover', (e) => {
-//     if (e.target === sassDiv || e.target === sassTextOverlay) {
-//         showOverlay(sassDiv, sassTextOverlay);
-//         return;
-//     }
-//     else {
-//         hideOverlay(sassDiv, sassTextOverlay);
-//         return;
-//     }
-// })
+    }
+]
+
+
+dashboardDiv.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        const index = e.target.getAttribute('data-index');
+        console.log(index);
+        buildModal(index);
+    }
+})
+// const modalButton = `<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+//                      <span aria-hidden="true">&times;</span>`;
+const buildModal = (index) => {
+    modalHeaderHTML = `${projects[index].title}`;
+    modalHTML = ``;
+}
