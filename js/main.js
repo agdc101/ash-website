5f5b48ca2021c80c76ff75eee35b8f40efe14f4c
Ashley Campbell <ashleycampbell@MacBook-Pro.lan>
2020-08-30 17:26:03 +0100

    modal footer update

--- js/main.js
+++ js/main.js
@@ -1,43 +1,44 @@
 
+const portfolioDiv = document.getElementById('portfolio');
 const modalHeader = document.querySelector('.modal-header');
 const modalBodyContent = document.querySelector('.modal-body');
-const portfolioDiv = document.getElementById('portfolio');
-
+const modalFooter = document.querySelector('.modal-footer');
+const modal = document.getElementById('exampleModal');
 const projects = [
     {
         title: 'SASS Web Layout',
-        imageLink: 'img/screenshots/dashboard.png',
+        imageLink: 'img/screenshots/modal-sass-screenshot.png',
         imageAlt: 'SASS. a CSS preprocessor, has many great features',
-        description: 'Description: Responsive SASS project demonstrating the power and flexibility of the SASS preprocessor. Featuring SASS loops, maps and functions creating a complex layout with a bare amount of code.',
+        description: 'Responsive SASS project demonstrating the power and flexibility of the SASS preprocessor. Featuring SASS loops, maps and functions creating a complex layout with a bare amount of code.',
         technologies: 'HTML, SASS.' 
     },
     {
-        title: 'Guess The Movie Title Game',
-        imageLink: 'img/screenshots/game.png',
+        title: 'Guess The Movie Game',
+        imageLink: 'img/screenshots/modal-game-screenshot.png',
         imageAlt: 'you can make really cool games with javascript',
-        description: 'Description: A javascript driven movie guessing game! The game allows the user 5 lives to guess the correct title. Give it a try!',
+        description: 'A javascript driven movie guessing game! The game allows the user 5 lives to guess the correct title. Give it a try!',
         technologies: 'HTML, CSS, Javascript.'
     },
     {
         title: 'Interactive Photo Gallery',
-        imageLink: 'img/screenshots/gallery.png',
+        imageLink: 'img/screenshots/modal-gallery-screenshot.png',
         imageAlt: 'wonderful photo gallery snapshot',
-        description: 'Description: Responsive interactive photo gallery driven using the Jquery framework. Photos can be filtered and searched using a search-bar and a Jquery plug-in is used for the carousel modal',
+        description: 'Responsive interactive photo gallery driven using the Jquery framework. Photos can be filtered and searched using a search-bar and a Jquery plug-in is used for the carousel modal',
         technologies: 'HTML, CSS, Jquery.'
 
     },
     {
         title: 'Web App Dashboard',
-        imageLink: 'img/screenshots/dash.png',
+        imageLink: 'img/screenshots/modal-dash-screenshot.png',
         imageAlt: 'a mockup design of a web app dashboard. many cool interactive features',
-        description: 'Description: Responsive dashboard web app featuring Javascript graphs, local storage, and utilises CSS grid for its layout. This dashboard mock-up shows how fun and interactive javascript can make websites.',
+        description: 'Responsive dashboard web app featuring Javascript graphs, local storage, and utilises CSS grid for its layout. This dashboard mock-up shows how fun and interactive javascript can make websites.',
         technologies: 'HTML, CSS, Jquery.'
     },
     {
         title: 'Employee Directory',
-        imageLink: 'img/screenshots/directory.png',
+        imageLink: 'img/screenshots/modal-direct-screenshot.png',
         imageAlt: 'using the fetch api to build an awesome directory',
-        description: 'Description: Responsive directory that communicates with a third-party API to retrieve 12 random employee profiles. Also features a modal which contains additional information on each employee.',
+        description: 'Responsive directory that communicates with a third-party API to retrieve 12 random employee profiles. Also features a modal which contains additional information on each employee.',
         technologies: 'HTML, CSS, Javascript.'
     }
 ]
@@ -45,17 +46,24 @@ const projects = [
 portfolioDiv.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
         let index = e.target.getAttribute('data-index');
+        modal.style.padding = '0';
         buildModal(index);
     }
 })
 
 const buildModal = (index) => {
     console.log(`${projects[index].title}`);
-    let headerHTML = `<h5>${projects[index].title}</h5>`;
+    let headerHTML = `<span>${projects[index].title}</span>`;
     headerHTML += `<button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>`;
     modalHeader.innerHTML = headerHTML;
 
     let bodyHTML = `<img id='modal-pic' src=${projects[index].imageLink} alt='${projects[index].imageAlt}'>`;
+    bodyHTML += `<p><strong>Description:</strong> ${projects[index].description}</p>`;
+    bodyHTML += `<span><strong>Technologies used:</strong> ${projects[index].technologies}</span>`;
     modalBodyContent.innerHTML = bodyHTML;
+
+    let footerIcons =  `<a href=''><img src='img/icons/github-icon.png' alt='github logo'></a>
+                        <a href=''><img src='img/icons/internet-icon.png' alt='the internet'></a>`;  
+    modalFooter.innerHTML = footerIcons;                
 }
