a4a353a336f6b941503b14848322458749be6ddd
Ashley Campbell <ashleycampbell@MacBook-Pro.lan>
2020-09-28 23:25:26 +0100

    contact formupdate

--- php/mail.php
+++ php/mail.php
@@ -3,7 +3,7 @@
     $email = $_POST['email'];
     $message = $_POST['message'];
     $formcontent="From: $name \n Message: $message";
-    $recipient = "agdc101@gmail.com";
+    $recipient = "ashley@campbellwebdev.com";
     $subject = "Contact Form";
     $mailheader = "From: $email \r\n";
     mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
