83d05ab8c463258f178cfdd90a0da0cb39304b1d
Ashley Campbell <ashleycampbell@MacBook-Pro.lan>
2020-09-30 17:46:14 +0100

    PHP changes

--- php/mail.php
+++ php/mail.php
@@ -1,11 +1,23 @@
+
 <?php 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $formcontent="From: $name \n Message: $message";
-    $recipient = "ashley@campbellwebdev.com";
-    $subject = "Contact Form";
-    $mailheader = "From: $email \r\n";
-    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-    echo "Thank You!";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "agdc101@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 ?>
+
+<html>
+    <head>
+        <link rel='stylesheet' href='../css/styles.css'>
+    </head>
+    <body>
+        <div class='phpDiv'>
+            <p>Thank you! I'll get back to very shortly!</p>
+            <a id='returnBtn' href="../index.html">Return To Home Page</a>
+        </div>
+    </body>
+</html>
